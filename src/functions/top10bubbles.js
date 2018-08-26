366efceb92a0521e1b468d38abab2309bc6be6d5
Roytangrb <roytang.rb@gmail.com>
2018-08-26 14:29:47 +0800

    top10 change to force simulation

--- src/functions/top10bubbles.js
+++ src/functions/top10bubbles.js
@@ -4,21 +4,35 @@ const sort = (data, radiusScale, simulation, bubbles)=>{
     //hide exit_group
     d3.select('.exit_group').selectAll('circle').attr('visibility', 'hidden')
     const top10 = data.map(d=>d['Budget']).sort((a, b)=>b-a).slice(0, 10)
-    bubbles
-        .filter(d=>d['Budget'] <top10[9])
-        .transition()
-        .duration(1000)
-        .attr('r', 0)
 
-    bubbles
-        .filter(d=>d['Budget'] >=top10[9])
-        .transition()
-        .duration(2000)
-        .attr('cy', d=>top10.indexOf(d['Budget']) < 5? -250:-100)
-        .attr('cx', d=>top10.indexOf(d['Budget']) < 5?(top10.indexOf(d['Budget'])+1) * 200 - 600 : (top10.indexOf(d['Budget'])-4) * 200 - 600)
+    const forceY = d3.forceY(d=>{
+        if (d['Budget'] >=top10[9]){
+            return top10.indexOf(d['Budget']) < 5? -250:-100
+        } else {
+            return 300
+        }
+    }).strength(d=>d['Budget'] >=top10[9]?0.1:0.01)
 
-    simulation.force('x').strength(0)
-    simulation.force('y').strength(0)
+    let gap_agg = -400
+    let gap = top10.map((d, i)=>{
+        let r = radiusScale(d)
+        if (i === 5) gap_agg = -400
+        gap_agg = gap_agg + r * 2 + 30
+        return gap_agg 
+    })
+    const forceX = d3.forceX(d=>{
+        if (d['Budget'] >=top10[9]){
+            let index = top10.indexOf(d['Budget'])
+            return index%5?gap[index-1]:-400
+        } else {
+            return 0
+        }
+    }).strength(d=>d['Budget'] >=top10[9]?0.1:0.01)
+
+    simulation.force("y", forceY)
+    simulation.force("x", forceX)
+                .alphaTarget(0.4)
+                .restart()
 }
 
 export default sort
